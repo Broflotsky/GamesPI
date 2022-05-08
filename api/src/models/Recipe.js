19d11286283e657f7d52253cb13f4401ec962c9a
Broflotsky <ivancho0@hotmail.com>
2022-05-08 10:23:39 -0500

    back done

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,42 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
-    name: {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {  
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    resume: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    score: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    healthyscore: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    steps: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    img: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    createdInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+  },{
+    timestamps: false
+});
 };
